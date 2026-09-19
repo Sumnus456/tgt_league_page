77823f7b92a2b23835039bd9a0607a76f6338eec
Sumnus456 <itssumnus@gmail.com>
2026-09-19 15:49:43 -0400

    Disable blog for TGT league

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -3,7 +3,7 @@ export const leagueID = "1338196860103299072"; // your league ID
 export const leagueName = "Tabletop Game Talk"; // your league name
 export const dues = 0;
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
-export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
+export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
 export const homepageText = `
